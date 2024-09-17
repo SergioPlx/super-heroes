b520a3c591c63e679e666f06b13461d48a60e35c
Sergio <sergio.castrogonzalez@plexus.es>
2024-09-17 08:52:57 +0200

    add routing and set http config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HeroListComponent } from './features/hero-list/hero-list.component';
+import { HeroDetailComponent } from './features/hero-detail/hero-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'heroList',
+        pathMatch: 'full'
+    },
+    {
+        path: 'heroList',
+        component: HeroListComponent
+    },
+    {
+        path: 'heroDetail',
+        component: HeroDetailComponent
+    }
+];
