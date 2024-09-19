d89c6ca9fc378cba6b6b1500b30b8e05c17ec6a6
Sergio <sergio.castrogonzalez@plexus.es>
2024-09-19 15:08:18 +0200

    add test for searcher

--- src/app/shared/components/searcher/searcher.component.spec.ts
+++ src/app/shared/components/searcher/searcher.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearcherComponent } from './searcher.component';
 
+const lText: string = 'heroes searcher';
+
 describe('SearcherComponent', () => {
   let component: SearcherComponent;
   let fixture: ComponentFixture<SearcherComponent>;
@@ -13,11 +15,21 @@ describe('SearcherComponent', () => {
     .compileComponents();
 
     fixture = TestBed.createComponent(SearcherComponent);
+  });
+
+  beforeEach(() => {    
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should emit search text', ()=> {
+    spyOn(component.voutput_onSearch, 'emit');
+    component.vSearchText = lText;
+    component.handleClickSearch()
+    expect(component.voutput_onSearch.emit).toHaveBeenCalled();
+  })
 });
