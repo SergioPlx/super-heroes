f5bb95c8fb309a3f3ce1d2030814927516b0a5ce
Sergio <sergio.castrogonzalez@plexus.es>
2024-09-19 15:18:25 +0200

    test searcher part 2

--- src/app/shared/components/searcher/searcher.component.spec.ts
+++ src/app/shared/components/searcher/searcher.component.spec.ts
@@ -4,7 +4,7 @@ import { SearcherComponent } from './searcher.component';
 
 const lText: string = 'heroes searcher';
 
-describe('SearcherComponent', () => {
+fdescribe('SearcherComponent', () => {
   let component: SearcherComponent;
   let fixture: ComponentFixture<SearcherComponent>;
 
@@ -31,5 +31,16 @@ describe('SearcherComponent', () => {
     component.vSearchText = lText;
     component.handleClickSearch()
     expect(component.voutput_onSearch.emit).toHaveBeenCalled();
-  })
+  });
+
+  it('should get input group', () => {
+    const el = fixture.nativeElement;
+    const inputGroup = el.querySelector('p-inputgroup')
+    const input = el.querySelector('input');    
+    const button = el.querySelector('button');
+
+    expect(input.placeholder).toBe('Search super hero');
+    expect(button).toBeTruthy();    
+    expect(inputGroup).toBeTruthy();
+  });  
 });
