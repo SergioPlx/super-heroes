a5d37cfad249c674fbaed8f0e9a2a79f8569f0ab
Sergio <sergio.castrogonzalez@plexus.es>
2024-09-20 13:55:20 +0200

    add test to upper case directive

--- src/app/shared/directives/text-uppercase/text-uppercase.directive.spec.ts
+++ src/app/shared/directives/text-uppercase/text-uppercase.directive.spec.ts
@@ -1,8 +1,37 @@
-/*import { TextUppercaseDirective } from './text-uppercase.directive';
+import { Component, DebugElement, ElementRef, inject } from '@angular/core';
+import { TextUppercaseDirective } from './text-uppercase.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
-describe('TextUppercaseDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TextUppercaseDirective();
-    expect(directive).toBeTruthy();
+@Component({
+  standalone: true,
+  template: `<input type="text" TextUppercase value='test'/>`
+})
+class TestTextUppercaseComponent{}
+
+fdescribe('TextUppercaseDirective', () => {  
+  let component: TestTextUppercaseComponent;
+  let fixture: ComponentFixture<TestTextUppercaseComponent>;
+  let inputEl: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        TextUppercaseDirective,
+        TestTextUppercaseComponent
+      ]
+    });      
+
+    fixture = TestBed.createComponent(TestTextUppercaseComponent);
+    component = fixture.componentInstance;
+    inputEl = fixture.debugElement.query(By.css('input'));
   });
-});*/
+
+  it('transform text', () => {
+    const directive = new TextUppercaseDirective(inputEl);    
+    directive.onInputText()
+    fixture.detectChanges();
+    const lTextUpper = inputEl.nativeElement.value;    
+    expect(inputEl.nativeElement.value).toBe(lTextUpper.toUpperCase());
+  });
+});
