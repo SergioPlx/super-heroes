ad8d7c5583a87378a3b332e056407b6749ca01b1
Sergio <sergio.castrogonzalez@plexus.es>
2024-09-20 12:43:14 +0200

    add test notification service

--- src/app/core/services/notifications/notification.service.spec.ts
+++ src/app/core/services/notifications/notification.service.spec.ts
@@ -5,6 +5,7 @@ import { MessageService } from 'primeng/api';
 
 describe('NotificationService', () => {
   let service: NotificationService;
+  let message: any;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,9 +14,34 @@ describe('NotificationService', () => {
       ]
     });
     service = TestBed.inject(NotificationService);
+    message = TestBed.inject(MessageService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add success message, success()', () => {
+    spyOn(message, 'add');
+    service.success('Test success');
+    expect(message.add).toHaveBeenCalled();
+  });
+
+  it('should add info message, info()', () => {
+    spyOn(message, 'add');
+    service.info('Test info');
+    expect(message.add).toHaveBeenCalled();
+  });
+
+  it('should add info message, warning()', () => {
+    spyOn(message, 'add');
+    service.warning('Test warning');
+    expect(message.add).toHaveBeenCalled();
+  });
+
+  it('should add error message, error()', () => {
+    spyOn(message, 'add');
+    service.error('Test error');
+    expect(message.add).toHaveBeenCalled();
   });
 });
