df8aae6fcf2316ee9b8736b9968cc4779eb8a652
Sergio <sergio.castrogonzalez@plexus.es>
2024-09-20 13:00:48 +0200

    add test to page not found component

--- src/app/features/page-not-found/page-not-found.component.spec.ts
+++ src/app/features/page-not-found/page-not-found.component.spec.ts
@@ -1,23 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PageNotFoundComponent } from './page-not-found.component';
+import { Router } from '@angular/router';
 
+// TODO: Replace router with router testing
 describe('PageNotFoundComponent', () => {
   let component: PageNotFoundComponent;
   let fixture: ComponentFixture<PageNotFoundComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PageNotFoundComponent]
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(PageNotFoundComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);    
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should route to parent path when click button, handleClick()', () => {
+    spyOn(router, 'navigate');
+    component.handleClick();
+    expect(router.navigate).toHaveBeenCalled();
+  });
+
+  it('should has a button to return home', () => {
+    const lButtonHome = fixture.nativeElement.querySelector('p-button');
+    expect(lButtonHome).toBeTruthy();
+  });
+
+  it('should has message not found', () => {
+    const lMessageText = fixture.nativeElement.querySelector('.text-message');    
+    expect(lMessageText.textContent).toEqual('PAGE NOT FOUND');
+  })
 });
