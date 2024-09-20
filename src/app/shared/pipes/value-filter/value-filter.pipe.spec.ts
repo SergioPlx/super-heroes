afa1e5b5ba1709d4a45ac47f4a1870961f274ab0
Sergio <sergio.castrogonzalez@plexus.es>
2024-09-20 11:44:07 +0200

    add value filter test

--- src/app/shared/pipes/value-filter/value-filter.pipe.spec.ts
+++ src/app/shared/pipes/value-filter/value-filter.pipe.spec.ts
@@ -1,8 +1,42 @@
+import { IModelCharacter } from '../../../interfaces/character/character.interface';
 import { ValueFilterPipe } from './value-filter.pipe';
 
-describe('ValueFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ValueFilterPipe();
+fdescribe('ValueFilterPipe', () => {
+
+  const pipe = new ValueFilterPipe();
+  const lstItems: IModelCharacter[] = [
+    {
+      id: 1,
+      name: 'Test 1',
+      description: 'Test 1 description',
+      image: ''
+    },
+    {
+      id: 2,
+      name: 'Character 2',
+      description: 'Character 2 description',
+      image: ''
+    }
+  ];
+
+  const lstVoidItems: IModelCharacter[] = [];
+  
+  it('create an instance', () => {    
     expect(pipe).toBeTruthy();
   });
+
+  it('should get value from array', () => {
+    const lNameToSearch: string = 'Test';
+    expect(pipe.transform(lstItems, 'name', lNameToSearch).length).toBe(1);
+  });
+
+  it('without text search should return all items', () => {
+    const lNameToSearch: string = '';
+    expect(pipe.transform(lstItems, 'name', lNameToSearch).length).toBe(lstItems.length);
+  })
+
+  it('should return all items', () => {
+    const lNameToSearch: string = 'Test';
+    expect(pipe.transform(lstVoidItems, 'name', 'lNameToSearch').length).toBe(lstVoidItems.length);
+  })
 });
