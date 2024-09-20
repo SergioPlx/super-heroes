f37cfd4e994dfe8df56ecfc9c3d354b65afb77b2
Sergio <sergio.castrogonzalez@plexus.es>
2024-09-20 12:14:32 +0200

    add test to handle click edit

--- src/app/shared/components/character-card/character-card.component.spec.ts
+++ src/app/shared/components/character-card/character-card.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CharacterCardComponent } from './character-card.component';
 
-describe('CharacterCardComponent', () => {
+fdescribe('CharacterCardComponent', () => {
   let component: CharacterCardComponent;
   let fixture: ComponentFixture<CharacterCardComponent>;
 
@@ -11,13 +11,23 @@ describe('CharacterCardComponent', () => {
       imports: [CharacterCardComponent]
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(CharacterCardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should emit edit hero when click edit button, handleClickEdit()', ()=> {
+    spyOn(component.voutput_onEdit, 'emit');  
+    const buttonEdit = fixture.nativeElement.querySelector('.p-button-success');    
+    buttonEdit.click();
+    expect(component.voutput_onEdit.emit).toHaveBeenCalled();
+  })
+
 });
