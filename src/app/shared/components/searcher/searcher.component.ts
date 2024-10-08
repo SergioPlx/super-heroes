ddbd2ae180107b7b82653b82c7063ea8b1f10dfe
Sergio <sergio.castrogonzalez@plexus.es>
2024-10-08 17:04:35 +0200

    add debouncer

--- src/app/shared/components/searcher/searcher.component.ts
+++ src/app/shared/components/searcher/searcher.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputTextModule } from 'primeng/inputtext';
 import { TextCapitalizeDirective } from '../../directives/text-capitalize/text-capitalize.directive';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'searcher',
@@ -20,17 +21,31 @@ import { TextCapitalizeDirective } from '../../directives/text-capitalize/text-c
   templateUrl: './searcher.component.html',
   styleUrl: './searcher.component.css'
 })
-export class SearcherComponent {
-  public vSearchText: string = '';
+export class SearcherComponent implements OnInit, OnDestroy {
+  
+  private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSubscription?: Subscription;
+
+  // public vSearchText: string = '';
 
   @Output() voutput_onSearch: EventEmitter<string> = new EventEmitter();
 
-  handleClickSearch(): void {      
-    this.voutput_onSearch.emit(this.vSearchText)
+  ngOnInit(): void {
+    this.debouncerSubscription = this.debouncer
+    .pipe(
+      debounceTime(500)
+    ).subscribe(value => this.voutput_onSearch.emit(value)) 
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSubscription?.unsubscribe();
+  }
+
+  handleClickSearch(pSearchTearm: string): void {      
+    this.debouncer.next(pSearchTearm);
   }
   
   handleClickReset(): void {
-    this.vSearchText = '';
-    this.handleClickSearch();
+    this.debouncer.next('');
   }
 }
